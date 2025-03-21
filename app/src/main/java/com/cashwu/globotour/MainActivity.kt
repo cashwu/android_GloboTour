71255e86f9a536def4112f33263fde1749acf232
Cash <cashwugeek@icloud.com>
2025-03-21 14:17:25 +0800

    ch02

--- app/src/main/java/com/cashwu/globotour/MainActivity.kt
+++ app/src/main/java/com/cashwu/globotour/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.cashwu.globotour
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.cashwu.globotour.ui.theme.GloboTourTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.appbar.MaterialToolbar
 
-class MainActivity : ComponentActivity() {
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar        : MaterialToolbar
+    private lateinit var navController  : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GloboTourTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        // Initialize Views
+        toolbar         = findViewById(R.id.activity_main_toolbar)
+
+        // Get NavHostFragment and NavController
+        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        navController   = navHostFrag.navController
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GloboTourTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    GloboTourTheme {
+//        Greeting("Android")
+//    }
+//}
